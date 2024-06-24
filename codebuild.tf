438808879f90673522289988a9bade14b0d8065a
herley <herley.shaori@gmail.com>
2024-06-24 23:04:11 +0700

    updated buildspec.

--- codebuild.tf
+++ codebuild.tf
@@ -58,3 +58,8 @@ resource "aws_iam_role_policy_attachment" "codebuild_s3_policy_attachment" {
   role       = aws_iam_role.codebuild_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "codebuild_ecr_policy_attachment" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
