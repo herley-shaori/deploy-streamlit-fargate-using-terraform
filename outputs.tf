8fa2e0938c2a8143b80463882afbaca44f3792c5
herley <herley.shaori@gmail.com>
2024-06-29 11:43:23 +0700

    reconfigured ecs service.

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,14 @@ output "public_subnet_id" {
   value = aws_subnet.public.id
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
 
 output "bucket_arn" {
   value = aws_s3_bucket.my_bucket.arn
+}
+
+output "ecs_task_definition" {
+  value = aws_ecs_task_definition.task
 }
--- vpc.tf
+++ vpc.tf
@@ -25,16 +25,6 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.32/27"
-  availability_zone = "ap-southeast-3b" # Specify the availability zone
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -51,17 +41,4 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "private-route-table"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
 }
